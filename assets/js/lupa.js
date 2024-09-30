52b42c141a8afce9939e27b4be226062b7e31898
Moacir Vieira <164024947+MoacirV@users.noreply.github.com>
2024-09-30 20:41:00 -0300

    atualizações

--- assets/js/lupa.js
+++ assets/js/lupa.js
@@ -1,13 +1,14 @@
-const professions = [
-    {
-      title: "Engenheiro de Software",
-      description: "Responsável por projetar, desenvolver e manter sistemas de software complexos."
-    },
-    {
-      title: "Médico",
-      description: "Profissional da saúde que diagnostica e trata doenças, além de promover a saúde geral."
-    },
-    // ... outros objetos
+const salgados = [
+    { nome: "Coxinha", preco: "R$ 7.90" },
+    { nome: "Pão de Queijo", preco: "R$ 7.90" },
+    { nome: "Empada de Frango", preco: "R$ 7.90" },
+    { nome: "Pastel de Carne", preco: "R$ 7.90" },
+    { nome: "Quiche de Alho-Poro", preco: "R$ 7.90" },
+    { nome: "Bolinho de Bacalhau", preco: "R$ 7.90" },
+    { nome: "Mini Pizzas", preco: "R$ 7.90" },
+    { nome: "Iscas de Frango Empanadas", preco: "R$ 7.90" },
+    { nome: "Tábua de Frios", preco: "R$ 7.90" },
+    { nome: "Canapés Variados", preco: "R$ 7.90" }
 ];
 
 const cardContainer = document.querySelector(".card-container");
@@ -19,8 +20,8 @@ const displayData = data => {
         data.forEach(e => {
             cardContainer.innerHTML += `
             <div class="card">
-                <h3>${e.title}</h3>    
-                <p>${e.description}</p>    
+                <h3>${e.nome}</h3>    
+                <p>${e.preco}</p>    
             </div>`;
         });
         cardContainer.style.display = "flex"; // Exibe o container quando há resultados
@@ -35,8 +36,8 @@ searchInput.addEventListener("keyup", (e) => {
     if (searchValue === "") {
         cardContainer.style.display = "none"; // Oculta o container se o campo de pesquisa estiver vazio
     } else {
-        const filteredProfessions = professions.filter(i => i.title.toLowerCase().includes(searchValue));
-        displayData(filteredProfessions);
+        const filteredSalgados = salgados.filter(i => i.nome.toLowerCase().includes(searchValue));
+        displayData(filteredSalgados);
     }
 });
 
